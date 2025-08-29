573909f35fe9db510e91b768344b321777221939
Joshua <gk.joshua.777@gmail.com>
2025-08-29 13:21:47 -0400

    Update page, navbutton,preferences and herosection

--- components/Preferences.tsx
+++ components/Preferences.tsx
@@ -3,7 +3,7 @@ import { Checkbox } from "@/components/ui/checkbox";
 
 export default function Preferences() {
   return (
-    <div className="flex items-center space-x-2 px-11 sm:px-27 py-14 sm:py-14 text-black font-extralight">
+    <div className="flex items-center space-x-2 px-9 sm:px-12 py-14 sm:py-29 text-black font-extralight">
       <Checkbox id="terms" className="w-5 h-5 border border-gray-400" /> {/* ✅ Bigger checkbox */}
       <label
         htmlFor="terms"
